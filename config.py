e938b94076123305c666658239f83f387c9e7162
Shrikant Narayankar <k1nnn18@gmail.com>
2021-05-05 21:32:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 districtLink = 'https://api.covid19india.org/csv/latest/district_wise.csv'
 districtVaccineDetailsLink = 'http://api.covid19india.org/csv/latest/cowin_vaccine_data_districtwise.csv'
 urlScrap = 'http://117.247.89.137:85/'
-urlAWS = 'http://34.207.234.40:8080/'
+urlAWS = 'http://13.233.88.175:8080/'
 style = """<style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
